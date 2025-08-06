3fed49b2d6b86ac83615a9762888a407fb782809
muralikrishna1111 <muralike1234@gmail.com>
2025-08-06 21:37:33 +0530

    Update app.java

--- app.java
+++ app.java
@@ -19,3 +19,17 @@ public class QuotientRemainder {
   }
 }
 Output:
+
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
